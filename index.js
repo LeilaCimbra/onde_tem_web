ba4af67d2e658118527bf4f70c39af8720cac6f9
Leila <lcimbra18@gmail.com>
2025-06-02 12:11:43 -0300

    Ultimas atualuizações com o login em página separada. Os próximos passos serão implantar media queries e interações.

---
+++ index.js
@@ -0,0 +1,14 @@
+/*const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("modal");
+  fade.classList.toggle("fade");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
+*/
